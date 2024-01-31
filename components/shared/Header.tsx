1cb94ae84f346434d059784654f4ab95fb3979e7
ADITYA SENAPATI <adityasenapati@ADITYAs-MacBook-Air.local>
2024-01-31 16:16:33 +0530

    added feature to get all events, update and delete events

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,7 +2,7 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
-// import NavItems from "./NavItems";
+import NavItems from "./NavItems";
 import MobileNav from "./MobileNav";
 
 const Header = () => {
@@ -18,11 +18,11 @@ const Header = () => {
           />
         </Link>
 
-        {/* <SignedIn>
+        <SignedIn>
           <nav className="md:flex-between hidden w-full max-w-xs">
             <NavItems />
           </nav>
-        </SignedIn> */}
+        </SignedIn>
 
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
